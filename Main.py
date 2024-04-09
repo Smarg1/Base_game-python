eb31cefb1cb523cd90458b76aa9fde0e7205601a
smarg1 <ayaanblr.hussain@gmail.com>
2024-04-09 13:58:00 +0530

    sound renamed

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from window import *
 if __name__ == '__main__':
     win = window()
-    win.audio.play_sound_3d('song.mp3',(0,0),(0,0))
+    win.audio.play_sound_3d('sound.mp3',(0,0),(0,0))
     win.run()
     
